7d6eb5dd543a11d529a59f86b6625c12eb416dcd
eivind <eivindskandsen@hotmail.no>
2020-11-09 17:02:50 +0100

    Lek

---
+++ src/KapittelEn/hjelpeMetoder.java
@@ -0,0 +1,13 @@
+package KapittelEn;
+
+public class hjelpeMetoder {
+    public static void main(String[] args) {
+
+    }
+
+    public static void bytt(int []a, int i, int j){
+        int temp=a[i];
+        a[i]=a[j];
+        
+    }
+}
